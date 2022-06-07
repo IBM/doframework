534f6e27482c7cf178502c2ecfadd79f8e6509b1
Orit Davidovich <orit.davidovich@protonmail.com>
2022-06-07 17:08:25 +0300

    new version number

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
 ]
 
 setup(name='doframework',
-version='0.1.2',
+version='0.1.3',
 description='A testing framework for data-driven decision-optimization algorithms.',
 long_description_content_type="text/markdown",
 long_description=open('README.md').read(),
